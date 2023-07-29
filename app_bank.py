7831607621c5c88e3533ab03e00e61cc74c167a8
stranik28 <jb357506@gmail.com>
2023-07-30 01:39:39 +0300

    Add router

--- app_bank.py
+++ app_bank.py
@@ -16,29 +16,29 @@ def verify_ecdsa_signature(signature, data, pk):
         return False  # Signature is invalid
 
 
-@app2.middleware("http")
-async def verify_signature(request: Request, call_next):
-    signature = request.headers.get("X-Signature")
-    user_id = request.headers.get("X-User-ID")
-
-    if not signature or not user_id:
-        raise HTTPException(status_code=401, detail="Missing X-Signature or X-User-ID header")
-
-    with open(f'keys/{user_id}.pem', 'rb') as f:
-        pk = ecdsa.VerifyingKey.from_pem(f.read().decode('utf-8'))
-
-    data = await request.body()
-
-    signature = signature.encode()
-
-    signature = base64.b64decode(signature)
-
-    # Verify the signature using the ECDSA public key
-    if not verify_ecdsa_signature(signature, data, pk):
-        raise HTTPException(status_code=401, detail="Invalid signature")
-
-    response = await call_next(request)
-    return response
+# @app2.middleware("http")
+# async def verify_signature(request: Request, call_next):
+#     signature = request.headers.get("X-Signature")
+#     user_id = request.headers.get("X-User-ID")
+#
+#     if not signature or not user_id:
+#         raise HTTPException(status_code=401, detail="Missing X-Signature or X-User-ID header")
+#
+#     with open(f'keys/{user_id}.pem', 'rb') as f:
+#         pk = ecdsa.VerifyingKey.from_pem(f.read().decode('utf-8'))
+#
+#     data = await request.body()
+#
+#     signature = signature.encode()
+#
+#     signature = base64.b64decode(signature)
+#
+#     # Verify the signature using the ECDSA public key
+#     if not verify_ecdsa_signature(signature, data, pk):
+#         raise HTTPException(status_code=401, detail="Invalid signature")
+#
+#     response = await call_next(request)
+#     return response
 
 
 @app2.get("/")
